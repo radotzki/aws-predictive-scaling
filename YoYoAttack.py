1776b4ab87142a03b3ee92ce9fc0e83b8a953775
Itay Radotzki <radotzki@gmail.com>
2019-03-02 19:08:53 +0200

    attack

--- YoYoAttack.py
+++ YoYoAttack.py
@@ -1,37 +1,61 @@
 import datetime
 import subprocess
+from subprocess import Popen
 import time
 
 import pytz
 import boto3
 
+AWS_ACCESS_KEY_ID='***'
+AWS_SECRET_ACCESS_KEY='***'
+AWS_DEFAULT_REGION='us-east-2'
+
 JMETER_YOYO_ATTACK_ON = "C:\\Users\\Administrator\\Desktop\\apache-jmeter-5.1\\apache-jmeter-5.1\\bin\\jmeter.bat -n -t C:\\Users\\Administrator\\Desktop\\apache-jmeter-5.1\\apache-jmeter-5.1\\bin\\templates\\Test_Plan_Yoyo_Attack.jmx -l C:\\Users\\Administrator\\Desktop\\results\\attack_results_log.csv -j C:\\Users\\Administrator\\Desktop\\results\\attack_monitor.txt"
 JMETER_USER_ON = "C:\\Users\\Administrator\\Desktop\\apache-jmeter-5.1\\apache-jmeter-5.1\\bin\\jmeter.bat -n -t C:\\Users\\Administrator\\Desktop\\apache-jmeter-5.1\\apache-jmeter-5.1\\bin\\templates\\Test_Plan_User.jmx -l C:\\Users\\Administrator\\Desktop\\results\\results_log.csv -j C:\\Users\\Administrator\\Desktop\\results\\monitor.txt"
+JMETER_USER_PROB_ON = "C:\\Users\\Administrator\\Desktop\\apache-jmeter-5.1\\apache-jmeter-5.1\\bin\\jmeter.bat -n -t C:\\Users\\Administrator\\Desktop\\apache-jmeter-5.1\\apache-jmeter-5.1\\bin\\templates\\Test_Plan_User.jmx -l C:\\Users\\Administrator\\Desktop\\results\\results_log_{prob}.csv -j C:\\Users\\Administrator\\Desktop\\results\\monitor_{prob}.txt"
+JMETER_STOP = "C:\\Users\\Administrator\\Desktop\\apache-jmeter-5.1\\apache-jmeter-5.1\\bin\\stoptest.cmd"
 
 g_probnum = 1
-
+stop_ports = {}
+stop_ports[4445] = False
+stop_ports[4446] = False
+stop_ports[4447] = False
 
 def jmeter_user_on(probnum=0):
-    returned_value = subprocess.call(JMETER_YOYO_ATTACK_ON, shell=True)  # returns the exit code in unix
+    returned_value = Popen(JMETER_USER_PROB_ON.format(prob=probnum), shell=True)  # returns the exit code in unix
     print('returned value:', returned_value)
-    # TODO: Should return the port number
-    return returned_value
+    # Return the port number
+    max_port = 0
+    for port in stop_ports:
+        max_port = port
+        if False == stop_ports[port]:
+            stop_ports[port] = True
+            return port
+    stop_ports[max_port+1] = True
+    return max_port+1
 
 
 def jmeter_user_off(port):
-    pass
-
+    returned_value = Popen(JMETER_STOP+" "+ str(port), shell=True)
+    print('returned value:', returned_value)
 
 def jmeter_attack_on():
-    returned_value = subprocess.call(JMETER_USER_ON, shell=True)  # returns the exit code in unix
+    returned_value = Popen(JMETER_YOYO_ATTACK_ON, shell=True)  # returns the exit code in unix
     print('returned value:', returned_value)
-    # TODO: Should return the port number
-    return returned_value
+    # Return the port number
+    max_port = 0
+    for port in stop_ports:
+        max_port = port
+        if False == stop_ports[port]:
+            stop_ports[port] = True
+            return port
+    stop_ports[max_port+1] = True
+    return max_port+1
 
 
 def jmeter_attack_off(port):
-    pass
-
+    returned_value = Popen(JMETER_STOP+" "+ str(port), shell=True)
+    print('returned value:', returned_value)
 
 def prob(prob_duration):
     global g_probnum
@@ -42,9 +66,9 @@ def prob(prob_duration):
 
 
 def get_amount_of_running_machines():
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.resource('ec2',region_name='us-east-2', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}, {'Name': 'key-name', 'Values':['group7']}])
-    return len(instances)
+    return len(list(instances))
 
 
 def wait_for_steady_state(amount_of_machine_at_steady_state, sleep_between_checks=20, with_prob=False):
@@ -105,6 +129,6 @@ def save_attack_log(attack_log, attack_log_filepath):
     print "attack log saved!"
 
 
-if __name__ == '__main__':
-    attack_log = yoyo_attack_with_probs(2, 1000)
+if _name_ == '_main_':
+    attack_log = yoyo_attack_with_probs(2, 60*5)
     save_attack_log(attack_log, 'C:\\Users\\Administrator\\Desktop\\results\\attack_log.txt')
